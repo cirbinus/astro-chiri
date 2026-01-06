7d25f567072e8f8d8a3662e3dd52693da9d264bf
cirbinus <136683012+cirbinus@users.noreply.github.com>
2026-01-06 10:35:28 +0800

    Update website URL in theme configuration

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.cirbinus.workers.dev/', // Site domain
+    website: 'https://abya.dpdns.org/', // Site domain
     title: 'CHIRI', // Site title
     author: '3ASH', // Author name
     description: 'Minimal blog built by Astro', // Site description
