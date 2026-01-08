d2c3398fc0113653b4c568caf478aff4ebd61932
cirbinus <136683012+cirbinus@users.noreply.github.com>
2026-01-08 08:12:44 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const themeConfig: ThemeConfig = {
     title: 'NoteDB', // Site title
     author: 'Abin', // Author name
     description: 'Minimal blog built by Astro', // Site description
-    language: 'en-US' // Default language
+    language: 'zh-CN' // Default language
   },
 
   // GENERAL SETTINGS ////////////////////////////////////////////////////////////////////////////////////
